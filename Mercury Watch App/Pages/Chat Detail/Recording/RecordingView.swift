2ed78f62ae62064d1b29513f5bda0d9c4cc7443e
Marco Tammaro <marcotammaro42@gmail.com>
2024-06-26 21:21:33 +0200

    Implemented audio recording and playing

--- Mercury Watch App/Pages/Chat Detail/Recording/RecordingView.swift
+++ Mercury Watch App/Pages/Chat Detail/Recording/RecordingView.swift
@@ -14,27 +14,69 @@ struct RecordingView: View {
     @StateObject var vm = RecordingViewModel()
     @Binding var isPresented: Bool
     
+    var elapsedTime: String {
+        let time = vm.elapsedTime
+        let seconds = Int(time.truncatingRemainder(dividingBy: 60))
+        let minutes = Int(time / 60)
+        return String(format:"%02d:%02d", minutes, seconds)
+    }
+    
+    var mainActionIcon: String {
+        switch vm.state {
+        case .recStarted:
+            return "square.fill"
+        case .recStopped, .playStopped:
+            return "play.fill"
+        case .playStarted:
+            return "pause.fill"
+        case .sending:
+            return "ellipsis"
+        }
+    }
+    
+    var mainActionTitle: String {
+        switch vm.state {
+        case .recStarted:
+            return "Stop"
+        case .recStopped, .playStopped:
+            return "Play"
+        case .playStarted:
+            return "Pause"
+        case .sending:
+            return "Loading"
+        }
+    }
+    
     var body: some View {
         
-        VStack {
-            chart
-        }
-        .navigationTitle("\(Int(vm.elapsedTime))")
+        chart
+        .navigationTitle(elapsedTime)
         .defaultScrollAnchor(.bottom)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button("Send", systemImage: "arrow.up") {
-                    
+                    if vm.state == .sending { return }
+                    vm.didPressSendButton()
                 }
                 .foregroundStyle(.white, .blue)
             }
+            
             ToolbarItemGroup(placement: .bottomBar) {
                 
-                Button("Record", systemImage: "square.fill") {
-                    vm.stopRecordingAudio()
+                if vm.state != .sending {
+                    Button(
+                        mainActionTitle,
+                        systemImage: mainActionIcon
+                    ) {
+                        vm.didPressMainAction()
+                    }
+                    .controlSize(.large)
+                    .foregroundStyle(.white, .blue)
+                    
+                } else {
+                    ProgressView()
+                        .controlSize(.large)
                 }
-                .controlSize(.large)
-                .foregroundStyle(.white, .blue)
                 
             }
         }
@@ -60,12 +102,7 @@ struct RecordingView: View {
     @ViewBuilder
     var chart: some View {
         
-        let paddedData = vm.waveformData + Array(
-            repeating: RecordingViewModel.zeroValue,
-            count: RecordingViewModel.chunks
-        )
-        
-        Chart(Array(paddedData.enumerated()), id: \.0) { index, magnitude in
+        Chart(Array(vm.waveformData.enumerated()), id: \.0) { index, magnitude in
             BarMark(
                 x: .value("Frequency", String(index)),
                 y: .value("Magnitude", magnitude),
@@ -81,6 +118,7 @@ struct RecordingView: View {
         .chartYAxis(.hidden)
         .chartYScale(domain: -RecordingViewModel.maxValue...RecordingViewModel.maxValue)
     }
+    
 }
 
 #Preview {
