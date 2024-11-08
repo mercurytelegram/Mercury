eca3b9890caa28019c39a4ccd67bf5317d1e51d5
Marco Tammaro <marcotammaro42@gmail.com>
2024-11-08 18:43:16 +0100

    VoiceNoteView refactoring

--- Mercury Watch App/Pages/ChatDetail/Subviews/Message/Contents/VoiceNoteView.swift
+++ Mercury Watch App/Pages/ChatDetail/Subviews/Message/Contents/VoiceNoteView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import TDLibKit
 import DSWaveformImageViews
+import DSWaveformImage
 
 struct VoiceNoteView: View {
     
@@ -15,18 +16,35 @@ struct VoiceNoteView: View {
     let isOutgoing: Bool
     let onPress: () -> Void
     
+    private let waveformConfig: Waveform.Configuration
+    private let replyForegroundColor: Color
+    private let replyBackgroundColor: Color
+    
     init(model: VoiceNoteModel, isOutgoing: Bool, onPress: @escaping () -> Void) {
         self.model = model
         self.isOutgoing = isOutgoing
         self.onPress = onPress
+        
+        self.replyForegroundColor = isOutgoing ? .white : .blue
+        self.replyBackgroundColor = isOutgoing ? .blue : .white
+        
+        self.waveformConfig = .init(
+            style: .striped(
+                .init(color: UIColor(replyForegroundColor))
+            ),
+            verticalScalingFactor: 0.4
+        )
     }
     
-    var replyForegroundColor: Color {
-        isOutgoing ? .white : .blue
+    private var actionIcon: String {
+        model.isPlaying ? "pause.fill" : "play.fill"
     }
     
-    var replyBackgroundColor: Color {
-        isOutgoing ? .blue : .white
+    private var elapsedTime: String {
+        let time = model.seconds
+        let seconds = Int(time % 60)
+        let minutes = Int(time / 60)
+        return String(format:"%02d:%02d", minutes, seconds)
     }
     
     var body: some View {
@@ -39,55 +57,87 @@ struct VoiceNoteView: View {
                     
                     ZStack {
                         ProgressView()
+                            .tint(replyBackgroundColor)
                             .opacity(model.isLoading ? 1 : 0)
+                        Image(systemName: actionIcon)
                             .foregroundStyle(replyBackgroundColor)
-                        Image(systemName: model.isPlaying ? "pause.fill" : "play.fill")
                             .opacity(model.isLoading ? 0 : 1)
-                            .foregroundStyle(replyBackgroundColor)
                     }
                     .font(.system(size: 24))
                     .padding(12)
                     .background(replyForegroundColor)
                     .clipShape(Circle())
                     
-                })
+                }
+            )
             .frame(width: 42, height: 42)
             .buttonStyle(.plain)
             
             VStack(alignment: .leading) {
                 
-                var elapsedTime: String {
-                    let time = model.seconds
-                    let seconds = Int(time % 60)
-                    let minutes = Int(time / 60)
-                    return String(format:"%02d:%02d", minutes, seconds)
-                }
-                
                 if let url = model.audioUrl {
-                    WaveformView(
-                        audioURL: url,
-                        configuration: .init(
-                            style: .striped(.init(color: UIColor(replyForegroundColor))),
-                            verticalScalingFactor: 0.4
-                        )
-                    )
+                    waveform(url)
                     .frame(height: 42, alignment: .leading)
-                }
                 
-                HStack {
-                    Text(elapsedTime)
-                        .font(.system(size: 15))
-                        .bold()
-                    
-                    if !model.isListened {
-                        Circle()
-                            .frame(width: 6, height: 6)
+                    HStack {
+                        Text(elapsedTime)
+                            .font(.system(size: 15))
+                            .bold()
+                        
+                        if !model.isListened {
+                            Circle()
+                                .frame(
+                                    width: 6,
+                                    height: 6
+                                )
+                        }
+                        
                     }
+                    .foregroundStyle(replyForegroundColor)
                     
+                } else {
+                    waveformPlaceholder()
+                        .frame(height: 42, alignment: .leading)
                 }
-                .foregroundStyle(replyForegroundColor)
-                
             }
+        }
+    }
+    
+    @ViewBuilder
+    func waveform(_ url: URL) -> some View {
+        WaveformView(
+            audioURL: url,
+            configuration: waveformConfig
+        )
+    }
+    
+    @ViewBuilder
+    func waveformPlaceholder() -> some View {
+        
+        let sampleCount = 320
+        
+        // Adjust speed for faster or slower oscillation
+        let oscillationSpeed = 1.0
+        
+        // Width of the "0" region to improve visibility
+        let zeroWidth = 15
+        
+        TimelineView(.periodic(from: .now, by: 1/10)) { time in
+           
+            // Calculate the oscillating position based on time
+            let timeInterval = time.date.timeIntervalSinceReferenceDate
+            let sinValue = sin(timeInterval * oscillationSpeed)
+            let position = Int((sinValue + 1) / 2 * Double(sampleCount - 1))
+
+            // Generate the samples array with a "0" region that moves back and forth
+            let samples: [Float] = (0..<sampleCount).map {
+                ($0 >= position && $0 < position + zeroWidth) ? 0.2 : 1
+            }
+
+            WaveformLiveCanvas(
+                samples: samples,
+                configuration: waveformConfig
+            )
         }
     }
 }
@@ -96,17 +146,43 @@ struct VoiceNoteModel {
     var audioUrl: URL?
     var isPlaying: Bool = false
     var isLoading: Bool
-    var isListened: Bool
+    var isListened: Bool = false
     var seconds: Int = 0
 }
 
-#Preview {
+#Preview("Loading") {
+    VoiceNoteView(
+        model: .init(
+            isLoading: true
+        ),
+        isOutgoing: true
+    ) {
+        print("Play/Pause")
+    }
+}
+
+#Preview("Ready") {
     VoiceNoteView(
         model: .init(
             audioUrl: Bundle.main.url(forResource: "audio_sample", withExtension: "m4a"),
             isLoading: false,
             isListened: false,
             seconds: 10
+        ),
+        isOutgoing: false
+    ) {
+        print("Play/Pause")
+    }
+}
+
+#Preview("Listening") {
+    VoiceNoteView(
+        model: .init(
+            audioUrl: Bundle.main.url(forResource: "audio_sample", withExtension: "m4a"),
+            isPlaying: true,
+            isLoading: false,
+            isListened: true,
+            seconds: 140
         ),
         isOutgoing: false
     ) {
